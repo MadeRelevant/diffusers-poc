394d81bdaacc35b17e093db3d2e5d8db0eec2367
cblokland <chris@maderelevant.com>
2024-01-24 15:45:01 +0100

    align with recommendations on using realvisxlv3.0

--- examples/text_to_image/generate.py
+++ examples/text_to_image/generate.py
@@ -1,4 +1,4 @@
-from diffusers import AutoPipelineForText2Image
+from diffusers import AutoPipelineForText2Image, DPMSolverMultistepScheduler
 import torch
 from PIL import Image
 import sys
@@ -10,9 +10,16 @@ if len(sys.argv) > 1:
 else:
     prompt = "At a cocktail bar having a drink in a beautiful dress, half bodyshot"
 
-pipeline = AutoPipelineForText2Image.from_pretrained("SG161222/RealVisXL_V3.0", torch_dtype=torch.float16).to("cuda")
+pipeline = (AutoPipelineForText2Image.from_pretrained("SG161222/RealVisXL_V3.0", torch_dtype=torch.float16))
+pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
+pipeline = pipeline.to("cuda")
+
 pipeline.load_lora_weights("output", weight_name="pytorch_lora_weights.safetensors")
-image = pipeline(prompt, num_inference_steps=30, guidance_scale=7.5).images[0]
+image = pipeline(prompt,
+                 num_inference_steps=30,
+                 guidance_scale=7.5,
+                 negative_prompt="(worst quality, low quality, illustration, 3d, 2d, painting, cartoons, sketch), open mouth"
+                 ).images[0]
 
 # Convert to PIL Image if it's not already (optional, depending on the output format of the pipeline)
 if not isinstance(image, Image.Image):
@@ -20,7 +27,7 @@ if not isinstance(image, Image.Image):
 
 # Generate a unique filename based on the current date and time
 timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-filename = f"output_image_{timestamp}.jpg"
+filename = f"output/output_image_{timestamp}.jpg"
 
 # Save the image
 image.save(filename)
