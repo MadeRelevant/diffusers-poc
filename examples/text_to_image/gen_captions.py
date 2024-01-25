1389b002d4556334e900dea90e8151c35e9c2bc3
cblokland <chris@maderelevant.com>
2024-01-25 11:07:44 +0100

    fixed captions for real now

--- examples/text_to_image/gen_captions.py
+++ examples/text_to_image/gen_captions.py
@@ -1,12 +1,15 @@
 import json
 import os
+import torch
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base", torch_dtype=torch.float16).to("cuda")
+
 
 # Directory containing your image files
 image_directory = "data"  # Update this to your directory path
-
-# The caption to be used for all images
-# TODO make caption with BLIP or some AI that can extra the caption from the image
-caption = "aifluencer doing a selfie pose"
 
 # List all image files in the directory
 image_filenames = [f for f in os.listdir(image_directory) if f.endswith(('.jpg', '.jpeg', '.JPG', '.JPEG'))]
@@ -14,7 +17,14 @@ image_filenames = [f for f in os.listdir(image_directory) if f.endswith(('.jpg',
 # Create metadata entries
 metadata_entries = []
 for filename in image_filenames:
-    metadata_entry = {"file_name": filename, "caption": caption}
+    # conditional image captioning
+    text = "a photo of"
+    inputs = processor(Image.open(os.path.join(image_directory, filename)), text, return_tensors="pt").to("cuda", torch.float16)
+
+    out = model.generate(**inputs)
+    caption = processor.decode(out[0], skip_special_tokens=True)
+
+    metadata_entry = {"file_name": filename, "caption": "aifluencer taking " + caption}
     metadata_entries.append(metadata_entry)
 
 # Write to metadata.jsonl file
